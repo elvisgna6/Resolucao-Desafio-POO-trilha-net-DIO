11c5a8d0d2cd2300251c2dc7edc8a1491ae9a388
Elvis Georgio <elvisgna_6@hotmail.com>
2024-01-09 16:10:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+// Exemplo de uso com as classes Nokia e iPhone
+        Smartphone nokia = new Nokia("Lumia", 64, "123456789012345", "91234-3216");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp", "Lumia", 64, "123456789012345", "91234-3216");
+        nokia.ReceberLigacao();
+
+        Console.WriteLine("\n");
+
+
+        Smartphone iphone = new Iphone("X", 128, "987654321098765", "97879-5678");
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram", "X", 128, "987654321098765", "97879-5678");
+        iphone.ReceberLigacao();
+
+       
+       
+     
